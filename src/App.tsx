a71da06c8062e465f30f2c311d622d2d7a09e1ce
TeamFormPro <jaysimac10@yahoo.com>
2025-09-07 19:16:26 -0400

    Prod test: Add AuthPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import AuthPanel from './components/AuthPanel'
 import Dashboard from './components/Dashboard'
 export default function App() {
   return (
     <div className="container" style={{padding:20}}>
       <h1>TeamForm Pro</h1>
+      <div style={{height:12}} />
+      <AuthPanel />
       <div style={{height:12}} />
       <Dashboard />
     </div>
